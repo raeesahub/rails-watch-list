3d6bec41a43afef34ab12ea6e3cf03488a61cfa5
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-05-27 16:40:46 +0100

    user can create new bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,28 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    # @movie = Movie.find(params[:movie_id])
+    @bookmark.list = @list
+    # @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
+private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
 end
