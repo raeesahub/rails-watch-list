1ab32f5f84836305567d74a7df096fb1594eed00
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-05-27 15:50:33 +0100

    seeds from moviedb api

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,5 +4,4 @@ class Bookmark < ApplicationRecord
 
   validates :comment, length: { minimum: 6 }
   validates :movie, uniqueness: { scope: :list }
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+movies = JSON.parse(open(url).read)
+movies_array = movies['results']
+
+puts "Creating movies..."
+
+movies_array.each do |movie|
+  poster_url = "https://image.tmdb.org/t/p/original#{movie['poster_path']}"
+  new_movie = Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: poster_url,
+    rating: movie['vote_average']
+  )
+
+  puts "Creating movie with id: #{new_movie.id}"
+end
