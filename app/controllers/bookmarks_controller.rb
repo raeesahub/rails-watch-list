645a7097624b6cb18246af596d8b2e91a189040b
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-08-19 23:20:23 +0100

    removed comments from bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,9 +7,7 @@ class BookmarksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    # @movie = Movie.find(params[:movie_id])
     @bookmark.list = @list
-    # @bookmark.movie = @movie
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -26,6 +24,6 @@ class BookmarksController < ApplicationController
 private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment)
   end
 end
